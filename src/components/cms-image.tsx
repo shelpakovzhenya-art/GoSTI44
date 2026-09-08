4d5784f04a4316d6015cb5e1a9e2dfdb5f5cd03c
shelpakovzhenya-art <250310858+shelpakovzhenya-art@users.noreply.github.com>
2026-09-09 00:29:52 +0300

    Soften photo tones and replace green casts with neutral shading

--- src/components/cms-image.tsx
+++ src/components/cms-image.tsx
@@ -6,5 +6,10 @@ import { useSite } from "./cms-context";
 export default function CmsImage(props: ImageProps) {
   const { images } = useSite();
   const override = typeof props.src === "string" ? images[props.src] : undefined;
-  return <Image {...props} src={override?.src || props.src} alt={props.alt === "" ? "" : override?.alt || props.alt}/>;
+  const source = typeof props.src === "string" ? props.src : "";
+  const tone = /\/images\/(hero|veranda)\.jpg$/.test(source) ? "outdoor"
+    : /\/images\/spa-sauna\.jpg$/.test(source) ? "sauna"
+    : /\/images\/(family|business|lux|spa-tea)(-bedroom|-living)?\.jpg$/.test(source) ? "interior"
+    : undefined;
+  return <Image {...props} data-photo-tone={tone} src={override?.src || props.src} alt={props.alt === "" ? "" : override?.alt || props.alt}/>;
 }
